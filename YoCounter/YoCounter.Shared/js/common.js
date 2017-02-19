ba3c733481d6a36681c69ec1b8af75c2aba7f894
Martin Řezník <martin@yosarin.net>
2017-02-19 01:30:09 +0100

    removing old data

--- YoCounter/YoCounter.Shared/js/common.js
+++
@@ -1,6 +0,0 @@
-﻿function recalculateColumns() {
-    var width = Math.floor(Math.max($('#counters').width() / Object.keys(Counter.list).length, 200));
-    var columns = Math.floor($('#counters').width() / width);
-    width = Math.floor(Math.max($('#counters').width() / columns, 200));
-    $('.counter').css('width', width + 'px');
-}
